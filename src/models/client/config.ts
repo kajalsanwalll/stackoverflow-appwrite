da1ed8cb1d2f79a001a5c1bd6b2f29d7ec78e37b
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-01-29 13:42:06 +0530

    add client configurations in src

--- src/models/client/config.ts
+++ src/models/client/config.ts
@@ -2,11 +2,16 @@ import env from "@/app/env";
 import { Client, Account, Avatars, Databases, Storage } from "appwrite";
 
 const client = new Client()
-    .setEndpoint('https://<REGION>.cloud.appwrite.io/v1') // Your API Endpoint
-    .setProject('<YOUR_PROJECT_ID>'); // Your project ID
+    .setEndpoint(env.appwrite.endpoint) // Your API Endpoint
+    .setProject(env.appwrite.projectId); // Your project ID
 
+
+const databases = new Databases(client) ;   
 const account = new Account(client);
+const avatars = new Avatars(client);
+const storage = new Storage(client);
 
-const result = await account.get();
 
-console.log(result);
+export {client, databases, account, avatars, storage}
+
+
